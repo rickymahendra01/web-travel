d233a5c2073f497af85b2826151c993a95e74cad
rickymahendra01 <rickymahendra465@gmail.com>
2022-10-29 16:08:56 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/detail', [DetailController::class, 'index'])->name('detail');
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
+Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout-success');
+
+
+
+Route::get('admin', [DashboardController::class, 'index'])->name('dashboard');
